ee32a92b7ea1efc1a67cbfc71977a0786c10d6c6
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2022-03-27 10:15:52 +0530

    Add files via upload

---
+++ screens/DailyPic.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class DailyPicScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>DailyPic Screen!</Text>
+            </View>
+        )
+    }
+}
